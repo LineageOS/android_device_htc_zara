f09b2592cd5426a749cb253a1c1e496130be604a
Brinly Taylor <uberlaggydarwin@gmail.com>
2014-12-21 06:20:48 +1030

    zara: fixup cm.mk
    cleanup build id.

--- cm.mk
+++ cm.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, device/htc/zara/full_zara.mk)
 
-PRODUCT_RELEASE_NAME := zara
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
@@ -12,7 +10,7 @@ $(call inherit-product, device/htc/zara/device.mk)
 PRODUCT_DEVICE := zara
 PRODUCT_NAME := cm_zara
 PRODUCT_BRAND := htc
-PRODUCT_MODEL := Desire 601 GSM
+PRODUCT_MODEL := Desire 601
 PRODUCT_MANUFACTURER := HTC
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=zara BUILD_ID=KOT49H BUILD_FINGERPRINT="htc/htc_europe/zara:4.4.2/KOT49H/334166.10:user/release-keys" PRIVATE_BUILD_DESC="5.11.401.10 CL334166 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT="htc/htc_europe/zara:4.4.2/KOT49H/334166.10:user/release-keys" PRIVATE_BUILD_DESC="5.11.401.10 CL334166 release-keys"
